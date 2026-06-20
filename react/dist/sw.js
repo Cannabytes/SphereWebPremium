6fd1f2da00acf17dae3d3f7f3468c49ee02e4852
Cannabytes <logan22f@gmail.com>
2026-06-20 20:32:47 +0300

    Релиз SphereWeb 3.2.16

--- react/dist/sw.js
+++ react/dist/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'sphereweb3-shell-v5'
+const CACHE_VERSION = 'sphereweb3-shell-v6'
 const STATIC_CACHE = `${CACHE_VERSION}:static`
 const API_CACHE = `${CACHE_VERSION}:api`
 const STATIC_ASSETS = [
