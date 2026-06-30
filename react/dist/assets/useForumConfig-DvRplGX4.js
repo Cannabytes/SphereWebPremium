91208c5f1e1f8c6806a48869013cf9b36efce1ad
Cannabytes <logan22f@gmail.com>
2026-06-30 11:55:49 +0300

    Релиз SphereWeb 3.2.74

---
+++ react/dist/assets/useForumConfig-DvRplGX4.js
@@ -0,0 +1,1 @@
+import{t as e}from"./useQuery-cI0YDZGC.js";import{t}from"./queryKeys-16VOb91W.js";import{t as n}from"./forum.api-CHgFhp7l.js";var r=300*1e3,i=(i=!0)=>e({queryKey:t.forum.config,queryFn:n.getConfig,staleTime:r,refetchOnWindowFocus:!1,enabled:i});export{i as t};
