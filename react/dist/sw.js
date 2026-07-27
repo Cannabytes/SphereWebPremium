0f283221dd56bbaeb2807263fc03673fa5cb2bd2
Cannabytes <logan22f@gmail.com>
2026-07-27 04:33:21 +0300

    Релиз SphereWeb 3.3.71

--- react/dist/sw.js
+++ react/dist/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'sphereweb3-shell-v7'
+const CACHE_VERSION = 'sphereweb3-shell-v8'
 const STATIC_CACHE = `${CACHE_VERSION}:static`
 const API_CACHE = `${CACHE_VERSION}:api`
 const STATIC_ASSETS = [
